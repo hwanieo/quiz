aed635a0bfe882eee1bf85b7d94afcd5ba7170d9
hwnii <hwniieo@gmail.com>
2024-07-23 18:25:32 +0900

    Answer 컴포넌트 분리 및 key를 활용한 인스턴스 재생성

---
+++ src/components/Answers.jsx
@@ -0,0 +1,46 @@
+import { useRef } from 'react';
+
+export default function Answers({
+  answers,
+  selectedAnswer,
+  answerState,
+  onSelect,
+}) {
+  const shuffledAnswers = useRef();
+
+  if (!shuffledAnswers.current) {
+    // 리렌더링 될 때마다 계속 섞음.
+    shuffledAnswers.current = [...answers];
+    // Math.random() - 0.5 이렇게 하면 -0.5 ~ 0.5로 음수, 양수가 랜덤으로 나옴
+    shuffledAnswers.current.sort(() => Math.random() - 0.5); // -1: 요소들이 뒤바뀜 | 1: 유지
+  }
+
+  return (
+    <ul id='answers'>
+      {shuffledAnswers.current.map((answer) => {
+        // 마지막 선택한 answer
+        const isSelected = selectedAnswer === answer;
+        let cssClasses = '';
+
+        if (answerState === 'answered' && isSelected) {
+          cssClasses = 'selected';
+        }
+
+        if (
+          (answerState === 'correct' || answerState === 'wrong') &&
+          isSelected
+        ) {
+          cssClasses = answerState;
+        }
+
+        return (
+          <li key={answer} className='answer'>
+            <button onClick={() => onSelect(answer)} className={cssClasses}>
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,6 +3,7 @@ import { useCallback, useState } from 'react';
 import QUESTIONS from '../questions';
 import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionTimer from './QuestionTimer';
+import Answers from './Answers';
 
 export default function Quiz() {
   const [answerState, setAnswerState] = useState('');
@@ -51,51 +52,25 @@ export default function Quiz() {
     );
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  // Math.random() - 0.5 이렇게 하면 -0.5 ~ 0.5로 음수, 양수가 랜덤으로 나옴
-  shuffledAnswers.sort(() => Math.random() - 0.5); // -1: 요소들이 뒤바뀜 | 1: 유지
-
   return (
     <div id='quiz'>
       {/* 상위 컴포넌트가 리렌더링 되어도 하위 컴포넌트의 인스턴스는 유지됨 | 기존의 인스턴스가 재사용됨 | 새로운 인스턴스를 생성하려면 key 사용 */}
       {/* 지금 내가 선택을 하면서 handleSelectAnswer 함수가 실행되면서 userAnswers에 내가 지금 선택한 answer가 담기고, 
           그 내부에서 answerState를 업데이트 하고, jsx에서는 그걸 가지고 jsx를 재평가함. 그 다음 문제로 넘어가기 전 찰나의 순간에 */}
-      <QuestionTimer
-        key={activeQuestionIndex}
-        timeout={10000}
-        onTimeout={handleSkipAnswer}
-      />
       <div id='question'>
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id='answers'>
-          {shuffledAnswers.map((answer) => {
-            // 마지막 선택한 answer
-            const isSelected = userAnswers[userAnswers.length - 1] === answer;
-            let cssClasses = '';
-
-            if (answerState === 'answered' && isSelected) {
-              cssClasses = 'selected';
-            }
-
-            if (
-              (answerState === 'correct' || answerState === 'wrong') &&
-              isSelected
-            ) {
-              cssClasses = answerState;
-            }
-
-            return (
-              <li key={answer} className='answer'>
-                <button
-                  onClick={() => handleSelectAnswer(answer)}
-                  className={cssClasses}
-                >
-                  {answer}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+        <Answers
+          key={activeQuestionIndex}
+          answers={QUESTIONS[activeQuestionIndex].answers}
+          selectedAnswer={userAnswers[userAnswers.length - 1]}
+          answerState={answerState}
+          onSelect={handleSelectAnswer}
+        />
       </div>
     </div>
   );
